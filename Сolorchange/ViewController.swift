5ed689ecf5cbd999acea26287b1c3358b993fdbc
Burenkovuser <nb2207@yandex.ru>
2019-07-26 22:11:30 +0700

    start UI, outlet and action app

--- Сolorchange/ViewController.swift
+++ Сolorchange/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabelOutlet: UILabel!
+    @IBOutlet weak var greenLabelOutlet: UILabel!
+    @IBOutlet weak var blueLabelOutlet: UILabel!
+    
+    @IBOutlet weak var redSlader: UISlider!
+    @IBOutlet weak var greenSlader: UISlider!
+    @IBOutlet weak var blueSlader: UISlider!
+    
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func redSladerAction() {
+    }
+    
+    @IBAction func greenSladerAction() {
+    }
+    
+    @IBAction func blueSladerAction() {
+    }
+    
 }
 
